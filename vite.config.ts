6e413bdae0b2671123d317108810b4d4eda67462
dean.gaudet <dean.gaudet@enverus.com>
2025-12-01 16:03:01 -0700

    - removing commented out code

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 5173,
-//     strictPort: true,
-//     allowedHosts: true,
-//   },
-//   build: {
-//     outDir: 'dist',
-//     sourcemap: true,
-//   },
-// })
-
 export default defineConfig(({ mode }) => {
   // Load environment variables that start with VITE_
   const env = loadEnv(mode, process.cwd())
